2fa694ce663a253028e7c6388477bfdff124a7b2
apocalipsistopo <90875653+apocalipsistopo@users.noreply.github.com>
2022-08-10 00:23:23 -0500

    Update instalador.bat

--- instalador.bat
+++ instalador.bat
@@ -1,6 +1,7 @@
 echo Instalador de la BDUniversidad
 echo Autor: Cadenillas Ñaccha Fernando
 echo 08/08/2022
+sqlcmd -S. -E -i BDUniversidad.sql
 sqlcmd -S. -E -i BDUniversidadPA.sql
-echo Se ejecuto correctamente la BDa
+echo Se ejecuto correctamente la BD
 pause
